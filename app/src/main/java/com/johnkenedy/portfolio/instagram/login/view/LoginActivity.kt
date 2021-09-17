01c9c1b8dad411a71ec78574e4cee1259e73a7a8
John Kenedy <john.kenedy232@gmail.com>
2021-09-16 22:10:47 -0300

    LoginActivity.kt (scrollView) and form Validation

--- app/src/main/java/com/johnkenedy/portfolio/instagram/login/view/LoginActivity.kt
+++ app/src/main/java/com/johnkenedy/portfolio/instagram/login/view/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.johnkenedy.portfolio.instagram.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputLayout
 import com.johnkenedy.portfolio.instagram.R
 
 class LoginActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
+
+    findViewById<TextInputLayout>(R.id.til_login_email)
+      .error = "Esse e-mail é invalido"
   }
 }
